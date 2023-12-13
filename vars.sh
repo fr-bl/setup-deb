b76442463955e494e1881f3a131883eeb20e34fc
frederick <git@blaesing.me>
2023-12-13 11:24:15 +0100

    add bins to path

--- vars.sh
+++ vars.sh
@@ -3,5 +3,6 @@ DIR=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
 export PKGS="$DIR/pkgs"
 export MYCONF="$DIR/config"
 export CONF="$HOME/.config"
+export BIN="$DIR/bin"
 export ROOT="$HOME"
 export USR="$ROOT/usr"
