46a55df65f698ed3a1a2d35bb260e8e58ce14c32
Frederick <153513571+fr-bl@users.noreply.github.com>
2023-12-14 09:12:09 +0100

    make bins executable

--- path.sh
+++ path.sh
@@ -2,6 +2,7 @@ DIR=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
 
 source "$DIR/vars.sh"
 
+chmod +x "$BIN/*"
 export PATH="$PATH:$BIN"
 export PATH="$PATH:$USR/bin/"
 for dir in $USR/share/*/; do
